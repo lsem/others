75f3abd8005890ec98bd171f62d98f8005fcf3a6
Lyubomyr Semkiv <lyubomyr.semkiv@hotmail.com>
2015-03-19 16:15:54 +0200

    Cosmetic changes in reverse-inplace

--- str_reverse_noif-for--inplace.cpp
+++ str_reverse_noif-for--inplace.cpp
@@ -1,45 +1,54 @@
-
 #include <cstdio>
 #include <cstdlib>
 #include <memory>
+#include <malloc.h>
+#include <cstring>
 
 //
-// TODO: Fix empty string case
+// TODO: Fix empty string case (using the same technique for branching)
 //
 
 
-void reverse(char *input, size_t size, size_t length);
+namespace implementation {
 
-void break_fn(char *input, size_t size, size_t length)
-{
-};
+    void reverse(char *input, size_t size, size_t length);
 
-void continue_fn(char *input, size_t size, size_t length)
-{
-    char nextChar = *(input + size);
-    
-    reverse(input, size, length);
-    
-    input[length - size - 1] = nextChar;
+    void break_fn(char *input, size_t size, size_t length)
+    {
+    };
+
+    void continue_fn(char *input, size_t size, size_t length)
+    {
+        char nextChar = *(input + size);
+
+        reverse(input, size, length);
+
+        input[length - size - 1] = nextChar;
+    }
+
+    typedef void(*ReverseFnType)(char *input, size_t size, size_t length);
+
+    void reverse(char *input, size_t size, size_t length)
+    {
+        ReverseFnType fns[2] = { continue_fn, break_fn };
+        fns[(length - size) / length](input, size - 1, length);
+    }
+
 }
 
-typedef void(*ReverseFnType)(char *input, size_t size,  size_t length);
-
-void reverse(char *input, size_t size, size_t length)
+void reverse_inplace(char *inputstr, size_t size)
 {
-    ReverseFnType fns[2] = { continue_fn, break_fn };
-    fns[(length - size) / length](input, size - 1, length);
+    implementation::reverse(inputstr, size, size);
 }
-
-#define STRING_SIZE(Str) (sizeof(Str) - 1)
 
 int main(int argc, char* argv[])
 {
     const char *input = "test";
-    char *buffer = new char[strlen(input)];
+
+    char *buffer = (char*) _alloca(strlen(input));
     strcpy(buffer, input);
 
-    reverse(buffer, strlen(buffer), strlen(buffer));
+    reverse_inplace(buffer, strlen(buffer));
     
     std::printf("result: '%s'\n", buffer);
 
